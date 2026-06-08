c627d52f4b3a8fbdad4092302fb47bb12d952516
Роман Коровин <roma.ps.05@mail.ru>
2026-06-08 13:19:57 +0500

    Fixes

--- src/filters-utils.js
+++ src/filters-utils.js
@@ -4,17 +4,17 @@ import { getCurrentDate } from './utils';
 
 function isFuturePoint(point){
   const date = dayjs(point.dateFrom);
-  return dayjs(date).isAfter(getCurrentDate());
+  return dayjs(date).isAfter(getCurrentDate(), 'day');
 }
 
 function isPastPoint(point){
   const date = dayjs(point.dateFrom);
-  return dayjs(date).isBefore(getCurrentDate());
+  return dayjs(date).isBefore(getCurrentDate(), 'day');
 }
 
 function isPresentPoint(point){
   const date = dayjs(point.dateFrom);
-  return dayjs(date).isSame(getCurrentDate());
+  return dayjs(date).isSame(getCurrentDate(), 'day');
 }
 
 export {isFuturePoint, isPastPoint, isPresentPoint};
