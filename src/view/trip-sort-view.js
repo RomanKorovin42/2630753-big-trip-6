63183746dd472a6ad962b8189756843bb66a266c
Роман Коровин <roma.ps.05@mail.ru>
2026-06-12 11:39:32 +0500

    Fix

--- src/view/trip-sort-view.js
+++ src/view/trip-sort-view.js
@@ -1,8 +1,6 @@
 import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 
 function getTripSortTemplate(currentSortType){
-
-
   return `          <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
             <div class="trip-sort__item  trip-sort__item--day">
               <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" ${currentSortType === 'sort-day' ? 'checked' : ''} >
